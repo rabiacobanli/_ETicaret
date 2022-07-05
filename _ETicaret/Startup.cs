5bd8f4577ba0c96ec163fbc0f4e788d8d31b37f0
Rabia ÇOBANLI <r.cobanli95@hotmail.com>
2022-07-05 17:49:19 +0300

    Add project files.

---
+++ _ETicaret/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_ETicaret.Startup))]
+namespace _ETicaret
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
